46b4db405479e8a052a29b345337410426c87cb6
Saurav Jaiswal <saurav14th@gmail.com>
2023-04-12 17:29:26 +0545

    services and solutions sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,11 @@ module.exports = {
     },
     extend: {
       colors: {
-        brightRed: "hsl(12, 88%, 59%)",
-        brightRedLight: "hsl(12, 88%, 69%)",
-        brightRedSupLight: "hsl(12, 88%, 95%)",
-        darkBlue: "hsl(228, 39%, 23%)",
-        darkGrayishBlue: "hsl(227, 12%, 61%)",
-        veryDarkBlue: "hsl(233, 12%, 13%)",
-        veryPaleRed: "hsl(13, 100%, 96%)",
-        veryLightGray: "hsl(0, 0%, 98%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+      },
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
       },
     },
   },
